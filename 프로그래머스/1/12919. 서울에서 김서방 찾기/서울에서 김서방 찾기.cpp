a602e66db88e97ff6f71b0eb5f97bf5ff9d764f6
Ayumudayo <129680598+Ayumudayo@users.noreply.github.com>
2024-08-30 10:08:51 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.04 ms, Memory: 3.84 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(vector<string> seoul) {    
+    auto it = find(seoul.begin(), seoul.end(), "Kim");    
+    int index = distance(seoul.begin(), it);
+    
+    return "김서방은 " + to_string(index) + "에 있다";
+}
