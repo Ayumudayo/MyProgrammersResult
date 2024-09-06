3a80fdb154b68d590adfd3731003a631a125ea55
Ayumudayo <129680598+Ayumudayo@users.noreply.github.com>
2024-09-06 11:48:35 +0900

    [level 2] Title: 괄호 회전하기, Time: 25.83 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,40 +1,30 @@
 function isValid(s) {
     const stack = [];
-    const map = new Map();
-    map.set('(', ')');
-    map.set('{', '}');
-    map.set('[', ']');
-
+    const map = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    };
     for (let char of s) {
-        if (map.has(char)) {
-            // 여는 괄호이면 스택에 추가
+        if (char in map) {
             stack.push(char);
         } else {
-            // 닫는 괄호가 스택의 맨 위 여는 괄호에 맞지 않으면 유효하지 않음
-            const top = stack.pop();
-            if (map.get(top) !== char) {
+            if (stack.length === 0 || map[stack.pop()] !== char) {
                 return false;
             }
         }
     }
-
-    // 모든 괄호가 짝을 이루면 스택이 비어있어야 함
     return stack.length === 0;
-}
-
-function rotate(str, num) {
-    if (num === 0)
-        return str;
-    num = num % str.length; // 회전 횟수를 문자열 길이로 나눈 나머지로 설정
-    return str.slice(num) + str.slice(0, num);
 }
 
 function solution(s) {
     let answer = 0;
+    let rotated = s + s; // 문자열을 두 번 연결하여 회전 효과 생성
     
-    for(let i = 0; i < s.length; i++) {
-        if(isValid(rotate(s, i)))
+    for (let i = 0; i < s.length; i++) {
+        if (isValid(rotated.slice(i, i + s.length))) {
             answer++;
+        }
     }
     
     return answer;
