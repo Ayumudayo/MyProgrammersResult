a541cc4ecae6191d3dcf7e9268256adfdceae21d
Ayumudayo <129680598+Ayumudayo@users.noreply.github.com>
2024-09-25 09:58:48 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 49.91 ms, Memory: 159 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -3,7 +3,9 @@ function solution(numbers) {
     const answer = new Array(numbers.length).fill(-1);
 
     for (let i = 0; i < numbers.length; i++) {
+        // 현재 숫자가 스택의 마지막 인덱스에 해당하는 숫자보다 클 때
         while (stack.length > 0 && numbers[i] > numbers[stack[stack.length - 1]]) {
+            // 인덱스로 각 값들을 비교한다
             const index = stack.pop();
             answer[index] = numbers[i];
         }
