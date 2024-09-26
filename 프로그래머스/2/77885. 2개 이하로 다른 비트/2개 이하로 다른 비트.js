100aea0dbef9bbb3174d68cc160f762ab4f5e15b
Ayumudayo <129680598+Ayumudayo@users.noreply.github.com>
2024-09-26 16:57:45 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 486.93 ms, Memory: 58.2 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.js
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.js
@@ -12,12 +12,14 @@ function solution(numbers) {
     // 각 숫자에 대해 f(x)를 계산
     return numbers.map((num) => {
         let x = BigInt(num);
+        
         if (x % 2n === 0n) {    // 짝수인 경우
             return Number(x + 1n);
         } else {    // 홀수인 경우      
             let c = countTrail(x);
-            // c가 1이여도 x + 1이 된다.
+            // c가 1이어도 x + 1이 된다.
             let increment = 1n << BigInt(c - 1);
+            
             return Number(x + increment);
         }
     });
