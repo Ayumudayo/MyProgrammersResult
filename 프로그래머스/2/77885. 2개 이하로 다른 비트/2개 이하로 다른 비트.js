f5b0a2ef2e9b6214e1adc68ee82a748a5fe433f3
Ayumudayo <129680598+Ayumudayo@users.noreply.github.com>
2024-09-26 15:00:00 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 573.24 ms, Memory: 58.9 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.js
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.js
@@ -1,28 +1,24 @@
-function solution(numbers) {
-    // 연속된 1의 개수를 세는 함수
-    function countTrailingOnes(x) {
-        let count = 0;
-        while ((x & 1n) === 1n) {
-            count += 1;
-            x >>= 1n;
-        }
-        return count;
+// 연속된 1의 개수를 세는 함수
+function countTrail(x) {
+    let count = 0;
+    while ((x & 1n) === 1n) {
+        count += 1;
+        x >>= 1n;
     }
+    return count;
+}
 
+function solution(numbers) {
     // 각 숫자에 대해 f(x)를 계산
-    const answer = numbers.map(num => {
+    return numbers.map((num) => {
         let x = BigInt(num);
-        if (x % 2n === 0n) {
-            // 짝수인 경우
+        if (x % 2n === 0n) {    // 짝수인 경우
             return Number(x + 1n);
-        } else {
-            // 홀수인 경우
-            let c = countTrailingOnes(x);
-            // c가 0인 경우는 없으므로 안전하게 c-1을 수행
+        } else {    // 홀수인 경우      
+            let c = countTrail(x);
+            // c가 1이여도 x + 1이 된다.
             let increment = 1n << BigInt(c - 1);
             return Number(x + increment);
         }
     });
-
-    return answer;
 }
