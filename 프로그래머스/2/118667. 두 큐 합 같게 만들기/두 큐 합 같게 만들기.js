0e7c2607652f20f3b518af038edb292391aafa9d
Ayumudayo <129680598+Ayumudayo@users.noreply.github.com>
2024-10-07 09:58:47 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 20.66 ms, Memory: 56.4 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -1,5 +1,6 @@
-// 합을 비교하며 같거나 클 때 까지 pop하고 상대에 push
-// 같으면 리턴
+// 합을 비교하며 같아질 때 까지 돌림
+// 상한을 초과하게 되면 불가능으로 판단
+// (q1.length + q2.length) * 2나, q1.length * 2(또는 3) 이 합리적
 
 function solution(q1, q2) {
     let sum1 = q1.reduce((sum, cur) => sum + cur, 0);
@@ -15,13 +16,12 @@ function solution(q1, q2) {
     let cnt = 0;
     let i = 0, j = 0;
     const qLen1 = q1.length;
-    const cycleLimit = (qLen1 + q2.length) * 2;
+    // const cycleLimit = (qLen1 + q2.length) * 2;  // 보다 엄격한 상한선
+    const cycleLimit = qLen1 * 3;   // qLen1 * 2는 케이스1을 통과할 수 없음
     const mergedQ = q1.concat(q2);
 
     while (cnt <= cycleLimit) {
-        if (sum1 === target) {
-            return cnt;
-        }
+        if (sum1 === target) return cnt;
 
         if (sum1 > target) {
             // q1에서 빼서 q2로
