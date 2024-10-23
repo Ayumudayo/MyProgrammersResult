20e26a6c496552d0d560c06fb92f3f60bf1b465f
Ayumudayo <129680598+Ayumudayo@users.noreply.github.com>
2024-10-23 10:48:09 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.js
@@ -0,0 +1,29 @@
+// 음수층은 없음
+// 하지만 음수층이 있는 것 처럼 생각 가능
+
+function solution(storey) {
+    let answer = 0;
+    
+    while (storey !== 0) {
+        let current = storey % 10; // 현재 자릿수        
+        let next = Math.floor((storey % 100) / 10); // 다음 자릿수
+        
+        if (current > 5) {
+            // 현재 자릿수가 5보다 크면 위로 올라가는 게 이득
+            answer += (10 - current);
+            storey += (10 - current);
+        } else if (current === 5 && next >= 5) {
+            // 현재 자릿수가 5이고 다음 자릿수가 5 이상이면 위로 올라가는 게 이득
+            answer += 5;
+            storey += 5;
+        } else {
+            // 그 외의 경우는 아래로 내려가는 게 이득
+            answer += current;
+        }
+        
+        // 다음 자릿수로 이동
+        storey = Math.floor(storey / 10);
+    }
+    
+    return answer;
+}
